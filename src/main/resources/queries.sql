cce0ae147834ea94125dc243d68871e175dc5c71
Marat <imd_90@mail.ru>
2020-11-20 14:22:43 +0300

    Removed the excess

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -2,5 +2,3 @@ SELECT m.id, m.content,
        (SELECT u.name FROM users u WHERE u.id = m.sender_id) sender,
        (SELECT u.name FROM users u WHERE u.id = m.recipient_id) recipient
 FROM messages m;
-
-DROP TABLE messages;
